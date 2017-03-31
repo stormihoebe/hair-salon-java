36e4c20457b58ea0436f1a4e605f2a9e33e8b26f
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-03-31 09:15:00 -0700

    add constructor for Stylist Class and test of stylist instantiates correctly

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,8 +1,14 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class Stylist {
+  private int id;
+  private String name;
+  private String description;
 
-  public Stylist() {
+  public Stylist(String name, String description) {
+    this.name = name;
+    this.description = description;
 
   }
 
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,6 +1,7 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class StylistTest {
 
@@ -12,9 +13,15 @@ public class StylistTest {
   @After
   public void tearDown() {
     try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
+      String sql = "DELETE FROM stylists *;";
       con.createQuery(sql).executeUpdate();
     }
   }
+
+  @Test
+    public void Stylist_instantiatesCorrectly_true() {
+      Stylist testStylist = new Stylist ("test name", "experienced stylist, specializes in dying/bleaching");
+      assertTrue(testStylist instanceof Stylist);
+    }
 
 }
