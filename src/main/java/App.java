47ff1802e95ac1e3c627eb48b63a515ff511db4c
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-03-31 09:50:15 -0700

    add new stylist form with route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,14 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/stylists/new", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          model.put("template", "templates/stylists-form.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
+
   }
 }
